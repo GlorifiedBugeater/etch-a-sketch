089809ba6cac947d70623ae5f0c55fbb5ff6e8dc
Vitor <vitor41197@gmail.com>
2025-12-07 14:10:43 -0300

    Done up to assignment objective 3
    -The script creates 16 grids
    -changes color when you hover over it

--- script.js
+++ script.js
@@ -3,9 +3,43 @@ function createChild (timesToRepeat) {
     const content = document.createElement("div")
 
     for (let i=0; i<timesToRepeat;i++){
-        content.classList.add("container")
+        console.log("hello",i)
+        const content = document.createElement("div")
+        content.classList.add("child")
+        content.setAttribute("id",("child"+i))
         container.appendChild(content)
+
+        
     }
 }
 
+
+function changeBackground() {
+
+}
+
 createChild(16)
+
+const hoverOver =document.querySelectorAll(".child")
+const hoverOut =document.querySelectorAll(".child")
+
+hoverOver.forEach((child) => {
+    child.addEventListener("mouseover", (e) => {
+        console.log("event OVER fired")
+        e.target.style.background = "blue" 
+    })
+})
+
+hoverOut.forEach((child) => {
+    child.addEventListener("mouseout", (e) => {
+        console.log("event OUT fired")
+        // e.target.style.background = "grey" 
+    })
+})
+
+// let hover = document.querySelectorAll(".child")
+// hover.mouseover = (event) => {console.log("test")}
+// const btn = document.querySelectorAll(".child");
+// btn.addEventListener("click", () => {
+//   alert("Hello World");
+// })
