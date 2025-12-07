6e780822d2714564f4bd68ee56abec9eed53aab9
Vitor <vitor41197@gmail.com>
2025-12-07 16:05:12 -0300

    Project completed
    -Grid resize implemented in dirty but working way

--- script.js
+++ script.js
@@ -14,11 +14,46 @@ function createChild (timesToRepeat) {
 }
 
 
-function changeBackground() {
 
-}
 
-createChild(16)
+createChild(100)
+
+
+const button = document.querySelector("#btn")
+button.addEventListener("click",(e) =>{
+
+    const kid = document.querySelectorAll(".child")
+    kid.forEach(child =>child.remove())
+
+    rowsQuantity = (prompt("How many rows?",4))
+    createChild(rowsQuantity**2)
+    newValue = 960/rowsQuantity+"px"
+    const childWidth = document.querySelectorAll(".child")
+    const childHeigth = document.querySelectorAll(".child")
+    childWidth.forEach((child) => {
+        child.style.width = newValue
+    })
+    childHeigth.forEach((child) => {
+        child.style.height = newValue
+    })
+
+    const hoverOver =document.querySelectorAll(".child")
+    const hoverOut =document.querySelectorAll(".child")
+
+    hoverOver.forEach((child) => {
+        child.addEventListener("mouseover", (e) => {
+        console.log("event OVER fired")
+        e.target.style.background = "blue" 
+    })
+})
+
+    hoverOut.forEach((child) => {
+        child.addEventListener("mouseout", (e) => {
+        console.log("event OUT fired")
+        // e.target.style.background = "grey" 
+    })
+})
+}) 
 
 const hoverOver =document.querySelectorAll(".child")
 const hoverOut =document.querySelectorAll(".child")
